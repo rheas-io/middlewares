531f6a81398f5c54e8d0760ccdc569f096f506bd
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-02 12:12:40 +0530

    removed build dir from git

--- build/classes/trimStrings.d.ts
+++
@@ -1,9 +0,0 @@
-import { TransformRequest } from "./transformRequest";
-export declare class TrimStrings extends TransformRequest {
-    /**
-     * Trim the given request value.
-     *
-     * @param value Field value
-     */
-    protected clean(value: string): any;
-}
