3cc767deb84b3b15dcbe8b08008660ea81fb5705
Kalesh Kaladharan <kalesh.13@gmail.com>
2020-09-11 21:34:10 +0530

    stable queued cookie dispatch middleware init

---
+++ src/global/addCookies.ts
@@ -0,0 +1,28 @@
+import { IRequest } from '@rheas/contracts/core/request';
+import { IResponse } from '@rheas/contracts/core/response';
+import { IRequestHandler } from '@rheas/contracts/routes';
+
+/**
+ * This middleware adds all the queued cookies to the response
+ * header.
+ *
+ * @param req
+ * @param res
+ * @param next
+ */
+async function handler(req: IRequest, res: IResponse, next: IRequestHandler) {
+    res = await next(req, res);
+
+    // Add queued cookies to response
+    const cookieHeaders: string[] = [];
+
+    Object.values(req.cookies().queuedCookies()).forEach((cookie) => {
+        cookieHeaders.push(cookie.toString());
+    });
+
+    res.setHeader('Set-Cookie', cookieHeaders);
+
+    return res;
+}
+
+export default handler;
